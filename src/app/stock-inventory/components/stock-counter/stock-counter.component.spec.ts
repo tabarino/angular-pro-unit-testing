b14314ebcd2dcbdd1dd39efa8da4fec9e212a50c
tabarino <tabarino@outlook.com>
2020-11-02 22:39:20 +0000

    69 - Testing Component Templates

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
@@ -1,9 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { StockCounterComponent } from './stock-counter.component';
 
 describe('StockCounterComponent', () => {
     let component: StockCounterComponent;
     let fixture: ComponentFixture<StockCounterComponent>;
+    let el: DebugElement;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -15,6 +18,7 @@ describe('StockCounterComponent', () => {
         fixture = TestBed.createComponent(StockCounterComponent);
         component = fixture.componentInstance;
         component.value = 0;
+        el = fixture.debugElement;
         fixture.detectChanges();
     });
 
@@ -63,5 +67,20 @@ describe('StockCounterComponent', () => {
         component.step = 100;
         component.increment();
         expect(component.changed.emit).toHaveBeenCalledWith(100);
+    });
+
+    it('should increment when the + button is clicked', () => {
+        el.query(By.css('button:first-child')).triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(component.value).toBe(1);
+        expect(el.query(By.css('p')).nativeElement.textContent).toBe('1');
+    });
+
+    it('should increment the value when the up arrow is pressed', () => {
+        const event = new Event('KeyboardEvent') as any;
+        event.code = 'ArrowUp';
+        el.query(By.css('.stock-counter > div > div')).triggerEventHandler('keydown', event);
+        fixture.detectChanges();
+        expect(component.value).toBe(1);
     });
 });
